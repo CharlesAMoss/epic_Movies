e1a2c52bffbcfda18b626bb9765ace616f4850d9
CharlesAMoss <charlesamoss@yahoo.com>
2015-10-02 11:44:22 -0700

    add input form and function

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -1,3 +1,24 @@
 flicks.controller('ReviewsCtrl', function ReviewsCtrl($scope){
-    
+
+    $scope.reviews = [{
+        title: "This is Spinal Tap is still better than most movies",
+        body: ""
+    },
+    {
+        title: "The Princes bride: The unreachable peak of quality",
+        body: ""
+    }];
+
+    $scope.addReview = function() {
+        $scope.reviews.push({
+            title: $scope.reviewTitle,
+            skill: $scope.reviewBody,
+
+        });
+        $scope.reviewTitle = null;
+        $scope.reviewBody = null;
+    };
+
+
+
 });
