6342f1bbe7e00dcf5cbe24c4b83185a35665d29c
CharlesAMoss <charlesamoss@yahoo.com>
2015-10-05 12:08:01 -0700

    add alternative query selectors to nightMode directive

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -1,7 +1,10 @@
 flicks.directive("nightMode", function($document) {
     return function(scope, element, attrs) {
         element.bind("click", function() {
-            $document.find("body").toggleClass(attrs.nightMode);
+        $document.find("body").toggleClass(attrs.nightMode);
+        // angular.element(document.querySelector('#wrapper')).toggleClass(attrs.nightMode);
+        // angular.element(elem.querySelector('.wrapper')).toggleClass(attrs.nightMode); 
+
         });
     };
 });
